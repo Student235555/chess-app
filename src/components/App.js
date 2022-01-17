1225b2b8ab45668c35f510aeca6318bd29951cfa
Marcin Pawlus <marcinpawlus97@gmail.com>
2022-01-17 13:58:21 +0100

    Add random move game logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
+import {useState} from 'react';
+import Chess from 'chess.js';
+import {Chessboard} from 'react-chessboard';
 import '../styles/App.css';
-import { Chessboard } from 'react-chessboard';
 
+export default function PlayRandomMoveEngine() {
+  const [game, setGame] = useState(new Chess());
 
-function App() {
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q' // always promote to a queen for example simplicity
+      });
+    });
+    if (move === null) return false; // illegal move
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
   return (
     <div className="App">
       <div className='chessboard'>
-        <Chessboard id="BasicBoard" />
+        <Chessboard id="BasicBoard" position={game.fen()} onPieceDrop={onDrop}/>
       </div>
     </div>
   );
 }
 
-export default App
+
